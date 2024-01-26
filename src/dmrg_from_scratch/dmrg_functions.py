a8c2b729cfefc812dc6e03cf0aad1213ad5a6936
jtcantin <josh_cantin@hotmail.com>
2024-01-25 17:21:29 -0700

    Add right_normalize_mps, mps_tensor_svd_right_normalized, build_random_mps, mps_inner_product, and start of drmg_main

---
+++ src/dmrg_from_scratch/dmrg_functions.py
@@ -0,0 +1,139 @@
+from typing import List
+
+import numpy as np
+
+
+def drmg_main(
+    num_sites: int = 4,
+    physical_dimension: int = 2,
+    bond_dimension: int = 3,
+    seed: int = 0,
+):
+    """Main function of the DMRG algorithm.
+    Structure follows the pseudocode in Fig. 19 of DOI:  https://doi.org/10.1140/epjb/s10051-023-00575-2
+    """
+    pass
+    # initialization
+
+    # Build random MPS
+    mps_ket = build_random_mps(
+        num_sites=num_sites,
+        physical_dimension=physical_dimension,
+        bond_dimension=bond_dimension,
+        seed=seed,
+    )
+
+    # Transform MPS to right-canonical form
+    mps_ket = right_normalize_mps(mps_ket)
+    # Set L[1] = R[1] = 1
+
+
+def build_random_mps(
+    num_sites: int = 4,
+    physical_dimension: int = 2,
+    bond_dimension: int = 3,
+    seed: int = 0,
+) -> List[np.ndarray]:
+    """Builds a random MPS with bond dimension bond_dimension"""
+    # Initialize MPS with random numbers
+    rng = np.random.default_rng(seed)
+    mps = []
+    for i in range(num_sites):
+        # First and last tensors have a dummy outer bond dimension
+        if i == 0:
+            mps.append(rng.random((physical_dimension, bond_dimension)))
+            mps[i] = np.expand_dims(mps[i], axis=0)
+        elif i == num_sites - 1:
+            mps.append(rng.random((bond_dimension, physical_dimension)))
+            mps[i] = np.expand_dims(mps[i], axis=-1)
+        else:
+            mps.append(rng.random((bond_dimension, physical_dimension, bond_dimension)))
+
+    # for tensor in mps:
+    #     print(tensor.shape)
+    #     print(tensor)
+
+    # # Normalize MPS
+    # mps_norm = mps_inner_product(mps, mps)
+    # print("MPS norm: ", mps_norm)
+    # mps = [tensor / np.sqrt(mps_norm) for tensor in mps]
+    # print(mps_inner_product(mps, mps))
+
+    # assert np.allclose(mps_inner_product(mps, mps), 1.0)
+    return mps
+
+
+def right_normalize_mps(mps: List[np.ndarray]) -> List[np.ndarray]:
+    """Transforms an MPS to right-canonical form
+    Follows Fig 11b of https://doi.org/10.1140/epjb/s10051-023-00575-2
+    mps indices are abg"""
+
+    for iiter in range(len(mps) - 1, -1, -1):
+        tensor = mps[iiter]
+        print("Tensor ", iiter)
+        print(tensor.shape)
+        # Do SVD
+        u, s, vh = mps_tensor_svd_right_normalized(tensor)
+        print("u shape: ", u.shape)
+        print("s shape: ", s.shape)
+        print("vh shape: ", vh.shape)
+
+        # Update tensor
+        mps[iiter] = vh
+        print("New tensor shape: ", mps[iiter].shape)
+
+        # Contract u and s to the left for all but the last tensor
+        # For the last tensor, u is a complex number of absolute value 1 and s is the norm of the MPS,
+        # so we just discard them to normalize the MPS and set the global phase to 1
+        if iiter > 0:
+            mps[iiter - 1] = np.einsum("gda,ae,ez->gdz", mps[iiter - 1], u, np.diag(s))
+            print("New MPS shape: ", mps[iiter - 1].shape)
+        else:
+            print("MPS norm: ", s[0])
+
+    return mps
+
+
+def mps_tensor_svd_right_normalized(tensor: np.ndarray) -> List[np.ndarray]:
+    """Transforms an MPS tensor to right-canonical form
+    Follows Fig 11b of https://doi.org/10.1140/epjb/s10051-023-00575-2
+    mps indices are abg"""
+    tensor_orig_shape = tensor.shape
+    # Fuse bg
+    tensor = np.reshape(tensor, (tensor.shape[0], -1), order="C")
+
+    # SVD
+    u, s, vh = np.linalg.svd(
+        tensor,
+        full_matrices=False,  # Means that the the "thin" or "reduced" SVD is computed;
+        # see 4.2 of https://doi.org/10.1140/epjb/s10051-023-00575-2 for more details
+        compute_uv=True,
+        hermitian=False,
+    )
+
+    # Split bg
+    vh = np.reshape(
+        vh, (s.shape[0], tensor_orig_shape[1], tensor_orig_shape[2]), order="C"
+    )
+
+    return [u, s, vh]
+
+
+def mps_inner_product(
+    mps_ket: List[np.ndarray], mps_bra: List[np.ndarray]
+) -> np.float_:
+    """Computes the inner product of two MPSs"""
+    # Take conjugate transpose of bra MPS
+    mps_bra = [np.conj(tensor) for tensor in mps_bra]
+    # Contract tensors using the zipper method
+    # See 4.3.1 and Fig. 9 of  https://doi.org/10.1140/epjb/s10051-023-00575-2
+    contraction_matrix = np.eye(1)
+    for iiter in range(len(mps_ket)):
+        ket_tensor = mps_ket[iiter]  # .copy()
+        bra_tensor = mps_bra[iiter]  # .copy()
+
+        c_ket_tensor = np.einsum("ab,agd->bgd", contraction_matrix, ket_tensor)
+        contraction_matrix = np.einsum("bgd,bge->de", c_ket_tensor, bra_tensor)
+
+    assert contraction_matrix.shape == (1, 1)
+    return contraction_matrix[0, 0]
